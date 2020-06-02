d6677bab6254c72caa156a7c88437697adf6d704
Italo Matos <attackdiamond@gmail.com>
2020-06-02 12:35:58 -0300

    definidas bordas redondas nos itens e corrigido erro no pubspec

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,28 +9,31 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(product.imageUrl,
-      fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
-          color:  Theme.of(context).accentColor,
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+          child: GridTile(
+        child: Image.network(product.imageUrl,
+        fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
             onPressed: () {},
             color:  Theme.of(context).accentColor,
-          ),
+            ),
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color:  Theme.of(context).accentColor,
+            ),
+        ),
+        
       ),
-      
     );
   }
 }
