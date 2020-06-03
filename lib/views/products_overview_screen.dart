fa2dbbc49ed9d164b3177fdb29e1f3c3ab5073c6
Italo Matos <attackdiamond@gmail.com>
2020-06-03 08:43:23 -0300

    refatoramento products_grid

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,28 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/models/products.dart';
-import 'package:shop/providers/products.dart';
-import 'package:shop/widgets/product_item.dart';
-
+import 'package:shop/widgets/products_grid.dart';
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<Product> loadedProducts = Provider.of<Products>(context).items;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
+
