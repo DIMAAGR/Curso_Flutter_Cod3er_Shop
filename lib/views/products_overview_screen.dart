7b57c90a44511e5e2a5b1ed530660478eed189e5
Italo Matos <attackdiamond@gmail.com>
2020-06-03 08:38:01 -0300

    adicionado mixin e utilizando providers

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/products.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
-
   @override
   Widget build(BuildContext context) {
+    final List<Product> loadedProducts = Provider.of<Products>(context).items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
